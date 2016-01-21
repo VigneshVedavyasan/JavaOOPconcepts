2f2e6a0c94ad2f0459811ca765bd8dcf4ef33f0a
tovigu <tovigu@gmail.com>
2016-01-21 16:09:10 +0530

    first commit

---
+++ Duck/src/MallardDuck.java
@@ -0,0 +1,12 @@
+
+public class MallardDuck extends Duck{
+	public MallardDuck()
+	{
+		quackBehavior=new Quack();
+		flyBehavior=new FlyWithWings();
+	}
+	public void display()
+	{
+		System.out.println("I am a real Mallard Duck. Quack! Quack!");
+	}
+}
---
+++ Duck/src/MiniDuckSimulator.java
@@ -0,0 +1,10 @@
+
+public class MiniDuckSimulator {
+	public static void main(String[] args)
+	{
+		Duck mallard= new MallardDuck();
+		mallard.performQuack();
+		mallard.performfly();
+		mallard.display();
+	}
+}
